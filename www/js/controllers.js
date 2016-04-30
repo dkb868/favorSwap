1b6c8998a25f1d235014134eed88b6ce91f18c88
Dmitri Brereton <mitrikyle@yahoo.com>
2016-04-30 16:20:38 -0700

    added some controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,39 @@
-angular.module('app.controllers', [])
-  
-.controller('favorManagementViewCtrl', function($scope) {
+angular.module('app.controllers', ['app.services'])
 
-})
-      
-.controller('favorListViewCtrl', function($scope) {
+  .controller('favorManagementViewCtrl', function() {
 
-})
-   
-.controller('favorViewCtrl', function($scope) {
+  })
 
-})
-   
-.controller('favorPostViewCtrl', function($scope) {
+  .controller('favorListViewCtrl', function(Favor) {
+    // get view model ref
+    var vm = this;
+    // Favor.all()
 
-})
- 
+    // set the favors (only incompleted favors)
+    vm.tasks = [
+      { title: "Collect coins"},
+      { title: "Eat shrooooms"},
+      {title: "get high enough"}
+    ];
+
+  })
+
+  .controller('favorViewCtrl', function($scope) {
+
+  })
+
+  .controller('favorPostViewCtrl', function(Favor) {
+    var vm = this;
+
+    // function to save favor
+    vm.saveFavor = function(){
+      // clear the 'error' messaeg
+      vm.message = '';
+
+      // use the create function in the favor service
+      /*Favor.create(vm.favorData)
+       .success(function(data){
+
+       })*/
+    }
+  });
