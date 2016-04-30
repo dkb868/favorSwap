7efe9a97a5b8230a4a5da5e04f58aa19644db590
Dmitri Brereton <mitrikyle@yahoo.com>
2016-04-30 13:02:58 -0700

    added base services code and changed defualt page to list

--- www/js/routes.js
+++ www/js/routes.js
@@ -7,10 +7,10 @@ angular.module('app.routes', ['ionicUIRouter'])
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
-    
-  
 
-      /* 
+
+
+      /*
     The IonicUIRouter.js UI-Router Modification is being used for this route.
     To navigate to this route, do NOT use a URL. Instead use one of the following:
       1) Using the ui-sref HTML attribute:
@@ -71,8 +71,8 @@ angular.module('app.routes', ['ionicUIRouter'])
     controller: 'favorPostViewCtrl'
   })
 
-$urlRouterProvider.otherwise('/page5')
+$urlRouterProvider.otherwise('/page6')
 
-  
+
 
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,10 +1,53 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
+.factory('Favor', [function(){
+
+  // create new object
+  var favorFactory = {};
+
+  // get all favors
+  favorFactory.all = function(){
+    // firebase shit goes here
+  };
+
+  // get one favor
+  favorFactory.get = function(id){
+    // firebase shit goes here
+  };
+
+  // create a favor
+  favorFactory.create = function(favorData){
+    // firebase shit goes here
+  };
+
+  // update a favor
+  favorFactory.update = function(id, favorData) {
+    // firebase shit
+  };
+
+  // deletea favor
+  favorFactory.delete = function(id){
+    // firebase shit
+  };
+
+  return favorFactory;
+
+}])
+
+.factory('Bid', [function(){
+
+  // craete object
+  var bidFactory = {};
+
+}]
+
+.factory('User', [function(){
+
+  // create object
+  var userFactory = {};
 
 }])
 
 .service('BlankService', [function(){
 
 }]);
-
